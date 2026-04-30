9cff5251ce2cee037732d7ee6a42fb5066ea5bf1
Porgelo <145523670+Porgelo@users.noreply.github.com>
2026-04-30 10:58:59 +0000

    reporter initial version

--- BusShuttleProject/BusShuttle/ConsoleUI.cs
+++ BusShuttleProject/BusShuttle/ConsoleUI.cs
@@ -59,7 +59,7 @@ public class ConsoleUI{
                     new SelectionPrompt<string>()
                         .Title("What do you want to do?")
                         .AddChoices(new[] {
-                            "add stop", "delete stop", "list all stops", "end"
+                            "show busiest stop","add stop", "delete stop", "list all stops", "end"
                         }));
 
                 if(command=="add stop") {
@@ -78,6 +78,10 @@ public class ConsoleUI{
                     }
                     AnsiConsole.Write(table);
                 } 
+                else if(command=="show busiest stop") {
+                    var result = Reporter.FindBusiestStop(dataManager.PassengerData);
+                    Console.WriteLine("The busiest stop is: "+result.Name);
+                }
                 
             } while (command != "end");
         }
